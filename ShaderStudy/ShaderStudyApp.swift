c073d5b359aaf03d92bcd640565363ba735240ee
b1t-ninja <nrueckner@outlook.de>
2026-05-21 11:34:13 +0200

    Initial Commit

---
+++ ShaderStudy/ShaderStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaderStudyApp.swift
+//  ShaderStudy
+//
+//  Created by Nico Rückner on 21.05.26.
+//
+
+import SwiftUI
+
+@main
+struct ShaderStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
